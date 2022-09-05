53b9a8cc2bf3bf248b652eaf4b6d3fc75f2a90bd
Florent Huck <florent@code-rhapsodie.fr>
2022-09-05 11:51:43 +0200

    debug admin entrypoint

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { HydraAdmin } from '@api-platform/admin';
 import platformVar from 'platformsh_variables';
-console.log(platformVar);
-let entrypoint = Object.keys(platformVar).find(url => url.endsWith('api'));
+console.log(Object.keys(platformVar));
+
+let entrypoint = Object.keys(platformVar).find(url => url.endsWith('/api'));
 
 if(!entrypoint) {
-  entrypoint = 'https://master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/api/';
+  entrypoint = 'https://master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/api';
 }
 entrypoint = entrypoint.substring(0, entrypoint.length - 1);
 console.log('entrypoint', entrypoint);
