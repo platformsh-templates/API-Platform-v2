e96bbc67ece1f63f2f7b8fce2c9cf5efba21c9a6
Florent Huck <florent@code-rhapsodie.fr>
2022-09-05 11:18:33 +0200

    adding route /api

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { HydraAdmin } from '@api-platform/admin';
 import platformVar from 'platformsh_variables';
 console.log(platformVar);
-let entrypoint = Object.keys(platformVar).find(url => url.endsWith('api/'));
+let entrypoint = Object.keys(platformVar).find(url => url.endsWith('api'));
 
 if(!entrypoint) {
   entrypoint = 'https://master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/api/';
