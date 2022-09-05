f3dd4c62bb6b988efc7b2950f8d81e212eec1985
Florent Huck <florent@code-rhapsodie.fr>
2022-09-05 11:12:06 +0200

    removing subdomain and adding prefix /api

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { HydraAdmin } from '@api-platform/admin';
 import platformVar from 'platformsh_variables';
 console.log(platformVar);
-let entrypoint = Object.keys(platformVar).find(url => url.startsWith('https://api.'));
+let entrypoint = Object.keys(platformVar).find(url => url.endsWith('api/'));
 
 if(!entrypoint) {
-  entrypoint = 'https://api.master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/';
+  entrypoint = 'https://master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/api/';
 }
 entrypoint = entrypoint.substring(0, entrypoint.length - 1);
 console.log('entrypoint', entrypoint);
