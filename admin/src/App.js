600aa827b7ba2ad150bee7611cd435b7e920955b
Florent Huck <florent@code-rhapsodie.fr>
2022-09-06 15:09:16 +0200

    changing api default url + removing useless debug trace

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,22 +2,14 @@ import React, { Component } from 'react';
 import { HydraAdmin } from '@api-platform/admin';
 import platformVar from 'platformsh_variables';
 
-console.log(Object.keys(platformVar));
-
-let entrypoint = Object.keys(platformVar).find(url => url.endsWith('/api'));
-
-console.log('entrypoint before tests', entrypoint);
+let entrypoint = Object.keys(platformVar).find(url => url.endsWith('/api/docs'));
 
 if(!entrypoint) {
   entrypoint = 'https://master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/api/docs/';
 }
 
 entrypoint = entrypoint.substring(0, entrypoint.length - 1);
-console.log('entrypoint final', entrypoint);
-
-// export default () => (
-//     <HydraAdmin entrypoint="{entrypoint}" />
-// );
+console.log('entrypoint', entrypoint);
 
 class App extends Component {
   render() {
