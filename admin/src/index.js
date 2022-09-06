70d651298d338bb43f8d42735cdb41c8345059c7
Florent Huck <florent@code-rhapsodie.fr>
2022-09-06 14:04:11 +0200

    remove service worker

--- admin/src/index.js
+++ admin/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,4 +10,4 @@ root.render(
         <App />
     </React.StrictMode>
 );
-registerServiceWorker();
+// registerServiceWorker();
