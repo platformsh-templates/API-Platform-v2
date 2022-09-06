f0fa121f0f99c323f8b496345c388008235c95a3
Florent Huck <florent@code-rhapsodie.fr>
2022-09-06 18:10:43 +0200

    admin: restarting from scratch

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import { HydraAdmin } from '@api-platform/admin';
-// import platformVar from 'platformsh_variables';
-//
-// let entrypoint = Object.keys(platformVar).find(url => url.endsWith('/api/'));
 
-// console.log(Object.keys(platformVar));
+
+
 
 let entrypoint = 'https://master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/api/';
 // if(!entrypoint) {
@@ -14,9 +12,9 @@ entrypoint = entrypoint.substring(0, entrypoint.length - 1);
 console.log('final entrypoint test', entrypoint);
 
 class App extends Component {
-  render() {
-    return <HydraAdmin entrypoint={entrypoint}/>
-  }
+    render() {
+        return <HydraAdmin entrypoint={entrypoint}/>
+    }
 }
 
 export default App;
