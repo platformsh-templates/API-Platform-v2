e1cf5d9d39fcb3ed84294fb8981c7d99298678aa
Florent Huck <florent@code-rhapsodie.fr>
2022-09-06 14:58:44 +0200

    resolving erros in app.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,9 +1,11 @@
 // import React, { Component } from 'react';
 import { HydraAdmin } from '@api-platform/admin';
 import platformVar from 'platformsh_variables';
+
 console.log(Object.keys(platformVar));
 
 let entrypoint = Object.keys(platformVar).find(url => url.endsWith('/api'));
+
 console.log('entrypoint before tests', entrypoint);
 
 if(!entrypoint) {
@@ -13,14 +15,14 @@ if(!entrypoint) {
 entrypoint = entrypoint.substring(0, entrypoint.length - 1);
 console.log('entrypoint final', entrypoint);
 
-export default () => (
-    <HydraAdmin entrypoint="{entrypoint}" />
-);
+// export default () => (
+//     <HydraAdmin entrypoint="{entrypoint}" />
+// );
 
-// class App extends Component {
-//   render() {
-//     return <HydraAdmin entrypoint={entrypoint}/>
-//   }
-// }
-//
-// export default App;
+class App extends Component {
+  render() {
+    return <HydraAdmin entrypoint={entrypoint}/>
+  }
+}
+
+export default App;
