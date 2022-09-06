f82a886f97f3b8cf45d9f2f96f64fef3137ef23b
Florent Huck <florent@code-rhapsodie.fr>
2022-09-06 14:38:07 +0200

    trying to solve admin

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import { HydraAdmin } from '@api-platform/admin';
 import platformVar from 'platformsh_variables';
 console.log(Object.keys(platformVar));
 
-// let entrypoint = Object.keys(platformVar).find(url => url.endsWith('/api'));
+let entrypoint = Object.keys(platformVar).find(url => url.endsWith('/api'));
+console.log('entrypoint before tests', entrypoint);
 
-let entrypoint = 'https://master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/api/docs/';
-// if(!entrypoint) {
-// }
-entrypoint = entrypoint.substring(0, entrypoint.length - 1);
-console.log('entrypoint toto', entrypoint);
-
-// export default () => (
-//     <HydraAdmin entrypoint="{entrypoint}" />
-// );
-
-class App extends Component {
-  render() {
-    return <HydraAdmin entrypoint={entrypoint}/>
-  }
+if(!entrypoint) {
+  entrypoint = 'https://master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/api/docs/';
 }
 
-export default App;
+entrypoint = entrypoint.substring(0, entrypoint.length - 1);
+console.log('entrypoint final', entrypoint);
+
+export default () => (
+    <HydraAdmin entrypoint="{entrypoint}" />
+);
+
+// class App extends Component {
+//   render() {
+//     return <HydraAdmin entrypoint={entrypoint}/>
+//   }
+// }
+//
+// export default App;
