fe3cd3913a6b310c879c1d9fc0782ef81c13e1ec
Florent Huck <florent@code-rhapsodie.fr>
2022-09-07 11:26:09 +0200

    api: changing nelmio routes and locations

--- admin/src/App.js
+++ admin/src/App.js
@@ -6,10 +6,6 @@ let entrypoint = Object.keys(platformVar).find(url => url.startsWith('https://ap
 
 console.log(entrypoint, Object.keys(platformVar));
 
-// const currentURL = window.location.href
-// let entrypoint = currentURL + 'api/'
-// console.log('entrypoint from current url', entrypoint)
-
 if(!entrypoint) { // default entrypoint
     entrypoint = 'https://api.master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/';
 }
