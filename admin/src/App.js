622e6a03dbfe308bd31b5e15732d864f177c3a02
Florent Huck <florent@code-rhapsodie.fr>
2022-09-07 12:10:19 +0200

    playing with routes

--- admin/src/App.js
+++ admin/src/App.js
@@ -11,10 +11,10 @@ console.log(entrypoint, Object.keys(platformVar));
 // console.log('entrypoint from current url', entrypoint)
 
 if(!entrypoint) { // default entrypoint
-    entrypoint = 'https://api.master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/';
+    entrypoint = 'https://api.fix-502-errors-wivvlli-7rmsoyfng4yww.fr-3.platformsh.site/api/';
 }
 entrypoint = entrypoint.substring(0, entrypoint.length - 1);
-console.log('final entrypoint test5', entrypoint);
+console.log('final entrypoint test6', entrypoint);
 
 class App extends Component {
     render() {
