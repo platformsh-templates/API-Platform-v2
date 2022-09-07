eb0623f5880b9ae42f3405ea7b53705bd142643e
Florent Huck <florent@code-rhapsodie.fr>
2022-09-07 08:53:03 +0200

    playing with api routes

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,10 +3,10 @@ import { HydraAdmin } from '@api-platform/admin';
 import platformVar from 'platformsh_variables';
 
 
-let entrypoint = Object.keys(platformVar).find(url => url.endsWith('/api'));
+let entrypoint = Object.keys(platformVar).find(url => url.endsWith('/api/'));
 
 if(!entrypoint) {
-    entrypoint = 'https://master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/api';
+    entrypoint = 'https://master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/api/';
 }
 // entrypoint = entrypoint.substring(0, entrypoint.length - 1);
 console.log('final entrypoint test', entrypoint);
