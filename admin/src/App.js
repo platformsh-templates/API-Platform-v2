d67881bf54dc9508060a3394db48888642784815
Florent Huck <florent@code-rhapsodie.fr>
2022-09-07 10:42:14 +0200

    going back to subdomains

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,19 +2,18 @@ import React, { Component } from 'react';
 import { HydraAdmin } from '@api-platform/admin';
 import platformVar from 'platformsh_variables';
 
+let entrypoint = Object.keys(platformVar).find(url => url.startsWith('https://api.'));
 
-let entrypoint_test = Object.keys(platformVar).find(url => url.endsWith('/api'));
+console.log(entrypoint, Object.keys(platformVar));
 
-console.log(entrypoint_test, Object.keys(platformVar));
-
-const currentURL = window.location.href
-let entrypoint = currentURL + 'api/'
-console.log('entrypoint from current url', entrypoint)
+// const currentURL = window.location.href
+// let entrypoint = currentURL + 'api/'
+// console.log('entrypoint from current url', entrypoint)
 
 if(!entrypoint) { // default entrypoint
-    entrypoint = 'https://master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/api/';
+    entrypoint = 'https://api.master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/';
 }
-// entrypoint = entrypoint.substring(0, entrypoint.length - 1);
+entrypoint = entrypoint.substring(0, entrypoint.length - 1);
 console.log('final entrypoint test3', entrypoint);
 
 class App extends Component {
