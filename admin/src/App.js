7de3cb7b9b5ec31ce0ed4486b83205c7dd19e1f4
Florent Huck <florent@code-rhapsodie.fr>
2022-09-07 10:34:11 +0200

    trying new routes

--- admin/src/App.js
+++ admin/src/App.js
@@ -9,12 +9,13 @@ console.log(entrypoint_test, Object.keys(platformVar));
 
 const currentURL = window.location.href
 let entrypoint = currentURL + 'api/'
+console.log('entrypoint from current url', entrypoint)
 
-if(!entrypoint) {
+if(!entrypoint) { // default entrypoint
     entrypoint = 'https://master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/api/';
 }
 // entrypoint = entrypoint.substring(0, entrypoint.length - 1);
-console.log('final entrypoint test2', entrypoint);
+console.log('final entrypoint test3', entrypoint);
 
 class App extends Component {
     render() {
