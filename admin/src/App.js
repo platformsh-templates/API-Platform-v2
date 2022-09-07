034b2c56a4d7e4e4518b4b15629b4374353fccde
Florent Huck <florent@code-rhapsodie.fr>
2022-09-07 09:06:28 +0200

    playing with route

--- admin/src/App.js
+++ admin/src/App.js
@@ -6,7 +6,7 @@ import platformVar from 'platformsh_variables';
 let entrypoint = Object.keys(platformVar).find(url => url.endsWith('/api/'));
 
 if(!entrypoint) {
-    entrypoint = 'https://master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/api/';
+    entrypoint = 'https://master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/api';
 }
 // entrypoint = entrypoint.substring(0, entrypoint.length - 1);
 console.log('final entrypoint test', entrypoint);
