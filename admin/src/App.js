1b20c450d57233d9877b5f67aa77dedd93d0758a
Florent Huck <florent@code-rhapsodie.fr>
2022-09-08 10:16:43 +0200

    admin: dynamic entrypoint

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,23 +2,21 @@ import React, { Component } from 'react';
 import { HydraAdmin } from '@api-platform/admin';
 // import platformVar from 'platformsh_variables';
 
-let entrypoint = 'https://master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/api';
+// let entrypoint = 'https://master-7rqtwti-7rmsoyfng4yww.fr-3.platformsh.site/api';
 
-// let entrypoint = Object.keys(platformVar).find(url => url.startsWith('https://api.'));
+let entrypoint = Object.keys(platformVar).find(url => url.startsWith('https://api.'));
+entrypoint = entrypoint + 'api/'
 
-// console.log(entrypoint, Object.keys(platformVar));
+console.log(entrypoint, Object.keys(platformVar));
 
 // // const currentURL = window.location.href
-// entrypoint = entrypoint + 'api/'
 // // console.log('entrypoint from current url', entrypoint)
 
 // if(!entrypoint) { // default entrypoint
 //     entrypoint = 'https://api.fix-502-errors-wivvlli-7rmsoyfng4yww.fr-3.platformsh.site/api/';
 // }
-// entrypoint = entrypoint.substring(0, entrypoint.length - 1);
-// console.log('final entrypoint test6', entrypoint);
-
-
+console.log('final entrypoint test6', entrypoint);
+entrypoint = entrypoint.substring(0, entrypoint.length - 1);
 
 class App extends Component {
     render() {
