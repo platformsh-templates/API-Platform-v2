6ed2f1c979bc802c8470facfee24e97daf2ce126
Florent Huck <florent@code-rhapsodie.fr>
2022-09-08 11:13:15 +0200

    api: remove public url

--- api/public/index.php
+++ api/public/index.php
@@ -7,6 +7,5 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 
 return function (array $context) {
-    var_dump($context['PUBLIC_URL']);
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
