23113331f372c8b6a46d0b7bf8b8324d7b3abb75
Florent Huck <florent@code-rhapsodie.fr>
2022-09-08 11:37:34 +0200

    remove useless code

--- admin/src/App.js
+++ admin/src/App.js
@@ -4,15 +4,14 @@ import { HydraAdmin } from '@api-platform/admin';
 let entrypoint = process.env.PUBLIC_URL;
 
 if(!entrypoint) { // default entrypoint to current url
-    entrypoint = window.location.href
+    entrypoint = window.location.href;
 }
 
-entrypoint = entrypoint + '/api'
-console.log('final entrypoint test6', entrypoint);
+entrypoint = entrypoint + '/api';
 
 class App extends Component {
     render() {
-        return <HydraAdmin entrypoint={entrypoint}/>
+        return <HydraAdmin entrypoint={entrypoint}/>;
     }
 }
 
