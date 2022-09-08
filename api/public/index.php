8c475d5e40cfb8ed8abae423f785d365da6d0a25
Florent Huck <florent@code-rhapsodie.fr>
2022-09-08 09:57:32 +0200

    debug dynamic env var

--- api/public/index.php
+++ api/public/index.php
@@ -4,6 +4,9 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+
+
 return function (array $context) {
+    var_dump($context['CORS_ALLOW_ORIGIN']);
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
